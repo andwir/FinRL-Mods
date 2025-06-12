1dc61c43dc543905ce0d57e98b8e9630403c0a65
andwir <43553597+andwir@users.noreply.github.com>
2025-06-13 04:45:44 +0700

    Update models.py

--- finrl/agents/rllib/models.py
+++ finrl/agents/rllib/models.py
@@ -2,13 +2,14 @@
 from __future__ import annotations
 
 import ray
-from ray.rllib.algorithms.a2c import a2c
+#AW from ray.rllib.algorithms.a2c import a2c
 from ray.rllib.algorithms.ddpg import ddpg
 from ray.rllib.algorithms.ppo import ppo
 from ray.rllib.algorithms.sac import sac
 from ray.rllib.algorithms.td3 import td3
 
-MODELS = {"a2c": a2c, "ddpg": ddpg, "td3": td3, "sac": sac, "ppo": ppo}
+#AW MODELS = {"a2c": a2c, "ddpg": ddpg, "td3": td3, "sac": sac, "ppo": ppo}
+MODELS = {"ddpg": ddpg, "td3": td3, "sac": sac, "ppo": ppo}
 
 
 # MODEL_KWARGS = {x: config.__dict__[f"{x.upper()}_PARAMS"] for x in MODELS.keys()}
